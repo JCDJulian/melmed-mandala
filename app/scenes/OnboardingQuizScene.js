221cb8e8efdc30cc8ca1eca0c5f03bf9ef8393cc
JCDJulian <julian.deocampo@gmail.com>
2017-04-15 14:32:32 -0700

    Pull getNextPage method into utility file.

--- app/scenes/OnboardingQuizScene.js
+++ app/scenes/OnboardingQuizScene.js
@@ -1,23 +1,11 @@
 import React, { Component } from 'react'
 import { View, Text, Button, StyleSheet } from 'react-native'
-import { Actions } from 'react-native-router-flux'
-import { WEDGES } from '../constants/Constants.js'
+import { getNextPage } from '../constants/UtilityMethods.js'
 
 export default class OnboardingQuizScene extends Component {
 
-  getNextPage() {
-    // If this is the last wedge, let nextPage go to mandala
-    if (this.props.wedge === WEDGES[WEDGES.length-1]) {
-      return () => Actions.mandala()
-    } else {
-      // Else get next wedge
-      let nextWedge = WEDGES[WEDGES.indexOf(this.props.wedge) + 1]
-      return () => Actions.onboardingQuiz({wedge: nextWedge})
-    }
-  }
-
   render() {
-    const nextPage = this.getNextPage()
+    const nextPage = getNextPage(this.props.wedge)
 
     return (
       <View style={styles.container}>
