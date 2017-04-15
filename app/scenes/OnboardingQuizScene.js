bf7b9e930a0741a40d0d1a55ade7b697356d7f5f
JCDJulian <julian.deocampo@gmail.com>
2017-04-15 15:31:38 -0700

    Add rate wedge scene

--- app/scenes/OnboardingQuizScene.js
+++ app/scenes/OnboardingQuizScene.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react'
 import { View, Text, Button, StyleSheet } from 'react-native'
 import { getNextPage } from '../constants/UtilityMethods.js'
+import { Actions } from 'react-native-router-flux'
 
 export default class OnboardingQuizScene extends Component {
 
   render() {
-    const nextPage = getNextPage(this.props.wedge)
-
     return (
       <View style={styles.container}>
         <Text>Hello world</Text>
         <Button
-          onPress={nextPage}
+          onPress={() => Actions.rateWedge({wedge: this.props.wedge})}
           title="Submit Assessment"
           accessibilityLabel="Submit assessment and move to mandala."
         />
-        <Text>{this.props.wedge}</Text>
+        <Text>Quizzing {this.props.wedge}</Text>
       </View>
     )
   }
