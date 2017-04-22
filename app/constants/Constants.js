a032e247ca23fed37526ee852932ad19626e6565
shwetachoudhary06 <shweta.choudhary@asu.edu>
2017-04-22 15:06:01 -0700

    constantupdate

--- app/constants/Constants.js
+++ app/constants/Constants.js
@@ -1,24 +1,188 @@
 export const WEDGES = ["Family Life",
-    "Relationships",
-    "Spirituality",
-    "Finances",
-    "Organization",
-    "Medical",
-    "Work",
-    "Social Skills",
-    "Exercise",
-    "Diet"
-  ]
+  "Relationships",
+  "Spirituality",
+  "Finances",
+  "Organization",
+  "Medical",
+  "Work",
+  "Social Skills",
+  "Exercise",
+  "Diet"
+]
 
 export const FAMILY_RECS = [
-    {
-      id: 0,
-      name: "Talk to your parents",
-      detail: "Talking more to your parents can help"
-    },
-    {
-      id: 1,
-      name: "Visit your brother",
-      detail: "Visitng siblings is always great"
-    }
+  {
+    id: 0,
+    name: "Talk to your parents",
+    detail: "Talking more to your parents can help",
+    image: '../../images/components/family.jpeg'
+  },
+  {
+    id: 1,
+    name: "Visit your brother",
+    detail: "Visitng siblings is always great"
+  }
 ]
+
+export const WEDGE_DATA = {
+  "Family Life": {
+    mainImage: "'../../images/components/family.jpeg'",
+    recs: [
+      {
+        id: 0,
+        name: "Talk to your parents",
+        detail: "Talking more to your parents can help",
+
+      },
+      {
+        id: 1,
+        name: "Visit your brother",
+        detail: "Visitng siblings is always great"
+      }
+    ]
+  },
+  "Relationships": {
+    mainImage: "'../../images/components/relationship.jpeg'",
+    recs: [
+      {
+        id: 0,
+        name: "Talk to your partner",
+        detail: "Talking more to your partners can help",
+
+      },
+      {
+        id: 1,
+        name: "Go out with your partner",
+        detail: "Doing social activites with partner is great"
+      }
+    ]
+  },
+  "Spirituality": {
+    mainImage: "'../../images/components/spirituality.jpeg'",
+    recs: [
+      {
+        id: 0,
+        name: "Talk to a priet",
+        detail: "Talking more to your priest",
+
+      },
+      {
+        id: 1,
+        name: "Temple",
+        detail: "Pray at a temple"
+      }
+    ]
+  },
+  "Finances": {
+    mainImage: "'../../images/components/finances.jpeg'",
+    recs: [
+      {
+        id: 0,
+        name: "Budget",
+        detail: "Make a budget",
+
+      },
+      {
+        id: 1,
+        name: "Savings Account",
+        detail: "Open a savings account"
+      }
+    ]
+  },
+  "Organization": {
+    mainImage: "'../../images/components/organization.jpeg'",
+    recs: [
+      {
+        id: 0,
+        name: "Organization1",
+        detail: "Organization11",
+
+      },
+      {
+        id: 1,
+        name: "Organization2",
+        detail: "Organization22"
+      }
+    ]
+  },
+  "Medical": {
+    mainImage: "'../../images/components/medicine.jpeg'",
+    recs: [
+      {
+        id: 0,
+        name: "Medicine Chart",
+        detail: "Make a chart",
+
+      },
+      {
+        id: 1,
+        name: "Check medicines",
+        detail: "Ask your doctor"
+      }
+    ]
+  },
+  "Work": {
+    mainImage: "'../../images/components/work.jpeg'",
+    recs: [
+      {
+        id: 0,
+        name: "Work1",
+        detail: "Work11",
+
+      },
+      {
+        id: 1,
+        name: "Work2",
+        detail: "Work22"
+      }
+    ]
+  },
+  "Social Skills": {
+    mainImage: "'../../images/components/SocialSkills.jpeg'",
+    recs: [
+      {
+        id: 0,
+        name: "Social1",
+        detail: "Social11",
+
+      },
+      {
+        id: 1,
+        name: "Social2",
+        detail: "Social22"
+      }
+    ]
+  },
+  "Exercise": {
+    mainImage: "'../../images/components/exercise.jpeg'",
+    recs: [
+      {
+        id: 0,
+        name: "Exercise1",
+        detail: "Exercise11",
+
+      },
+      {
+        id: 1,
+        name: "Exercise2",
+        detail: "Exercise22"
+      }
+    ]
+  },
+  "Diet": {
+    mainImage: "'../../images/components/nutrition.jpeg'",
+    recs: [
+      {
+        id: 0,
+        name: "Nutrition1",
+        detail: "Nutrition11",
+
+      },
+      {
+        id: 1,
+        name: "Nutrition2",
+        detail: "Nutrition22"
+      }
+    ]
+  }
+}
