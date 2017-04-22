f514ce9a589f929936cfa8c4ea947d583a5272fc
JCDJulian <julian.deocampo@gmail.com>
2017-04-22 13:44:04 -0700

    Some cleaning up file names

--- app/scenes/LaunchScene.js
+++ app/scenes/LaunchScene.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import { Actions } from 'react-native-router-flux'
 import LaunchContainer from '../containers/LaunchContainer'
 
 export default class LaunchScene extends Component {
