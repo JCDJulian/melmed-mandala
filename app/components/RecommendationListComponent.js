c7e6d0dd384f6050ceff0a42ab4d2b0f8123373e
JCDJulian <julian.deocampo@gmail.com>
2017-04-25 13:30:01 -0700

    Dynamically load image for rec

--- app/components/RecommendationListComponent.js
+++ app/components/RecommendationListComponent.js
@@ -11,12 +11,47 @@ export default class RecommendationListComponent extends Component {
       dataSource: ds.cloneWithRows(WEDGE_DATA[this.props.selectedWedge]["recs"])
     }
 
-    console.log(store.getState())
+    var headerImage = {}
+
+    switch (this.props.selectedWedge) {
+      case "Family Life":
+        headerImage = require('../../images/components/family.jpeg')
+        break;
+      case "Relationships":
+        headerImage = require('../../images/components/relationship.jpeg')
+        break;
+      case "Spirituality":
+        headerImage = require('../../images/components/spirituality.jpg')
+        break;
+      case "Finances":
+        headerImage = require('../../images/components/finances.jpeg')
+        break;
+      case "Organization":
+        headerImage = require('../../images/components/organization.jpg')
+        break;
+      case "Medical":
+        headerImage = require('../../images/components/medicine.jpeg')
+        break;
+      case "Work":
+        headerImage = require('../../images/components/work.jpg')
+        break;
+      case "Social Skills":
+        headerImage = require('../../images/components/SocialSkills.jpg')
+        break;
+      case "Exercise":
+        headerImage = require('../../images/components/exercise.jpg')
+        break;
+      case "Diet":
+        headerImage = require('../../images/components/nutrition.jpeg')
+        break;
+    }
+    console.log(this.props.selectedWedge)
+    console.log(headerImage)
     return (
     <View style={styles.wrapper}>
       <ListView
         dataSource={this.state.dataSource}
-        renderHeader={(rowData) => <Image source = {require('../../images/components/family.jpeg')} style = {styles.containerImage}/>}
+        renderHeader={(rowData) => <Image source = {headerImage} style = {styles.containerImage}/>}
         renderRow={(rowData) =>
           <TouchableHighlight
             onPress={
